df39fd6bf156dbf1657ca8127afb1d64fe85b651
gavin771 <gavin.samuels10@gmail.com>
2018-03-02 23:09:59 -0800

    Added ui to show cars list

--- src/containers/CarsList.js
+++ src/containers/CarsList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
 
 class CarsList extends Component {
 
@@ -7,9 +8,15 @@ class CarsList extends Component {
     if (cars) {
       return cars.map((c) => {
         return (
-          <div className="car_item" key={c.id}>
-            {c.model}
-          </div>
+          <Link to={`/cars/${c.id}`} className="car_item" key={c.id}>
+            <div className="left">
+              <img src={`/images/${c.image}`} alt={c.name} />
+            </div>
+            <div className="right">
+              <h4>{c.model}</h4>
+              <h6>{c.brand}</h6>
+            </div>
+          </Link>
         )
       })
     }
