075fcc382b7d152840ff64ed14ae0c3d134ff2d2
gavin771 <gavin.samuels10@gmail.com>
2018-03-02 21:18:47 -0800

    Added base funcitons to handle searching

--- src/containers/Search.js
+++ src/containers/Search.js
@@ -1,11 +1,34 @@
 import React, { Component } from 'react'
 
 class Search extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      keyword: ''
+    }
+  }
+
+  searchCars = (e) => {
+    e.preventDefault();
+    console.log(this.state)
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      keyword: e.target.value
+    })
+  }
+
+  componentDidMount () {
+    console.log(this.state);
+  }
+
   render () {
     return (
       <div className="main_search">
-        <form>
-          <input type="text" />
+        <form onSubmit={this.searchCars}>
+          <input type="text" value={this.state.keyword} onChange={this.handleChange} />
         </form>
       </div>
     )
