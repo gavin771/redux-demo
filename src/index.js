c18c02f56c21878f9450cf01d42167f00d569148
gavin771 <gavin.samuels10@gmail.com>
2018-03-02 21:24:13 -0800

    Added base files for adding reducers

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route } from 'react-router-dom';
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux';
+
+import reducers from './reducers'
 import registerServiceWorker from './registerServiceWorker';
 import './app.css';
 
 import App from './components/App';
 import Car from './components/Car'
 
+const createStoreWithMiddleware = applyMiddleware()(createStore)
+
 ReactDOM.render(
-  <BrowserRouter>
-    <div>
-      <Route exact path='/' component={App}></Route>
-      <Route path='/car/:id' component={Car}></Route>
-    </div>
-  </BrowserRouter>, document.getElementById('root'));
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <BrowserRouter>
+      <div>
+        <Route exact path='/' component={App}></Route>
+        <Route path='/car/:id' component={Car}></Route>
+      </div>
+    </BrowserRouter>
+  </Provider>, document.getElementById('root'));
 registerServiceWorker();
